1592d326fb6c91b7936346c417d0dc689ca88b16
arizabruno <brunoariza@outlook.com>
2023-10-31 17:15:50 -0300

    Fixed retrieving image from url step

--- routes/imageProcessingRoute.js
+++ routes/imageProcessingRoute.js
@@ -1,42 +1,46 @@
 import express from "express";
 import {filterImageFromURL, deleteLocalFiles} from "../util/util.js";
-import fs from 'fs';
+import axios from "axios";
 
 export const router = express.Router();
 
 router.get("/filteredimage", async (req, res) => {
 
+    let errorMsg = null;
+
     let  {image_url} = req.query;
 
     if(!image_url) {
-        const invalidUrl = "Invalid Image URL";
-        console.error(invalidUrl);
-        res.status(422).json(invalidUrl);
+        errorMsg = "Invalid Image URL";
+        console.error(errorMsg);
+        res.status(422).json(errorMsg);
     }
 
-    const filteredImage = await filterImageFromURL(image_url)
+    const imageData = await axios.get(image_url, { responseType: 'arraybuffer' })
+    .then(response => Buffer.from(response.data))
+    .catch(error => {
+        errorMsg = "Error retrieving image from URL";
+        console.error(errorMsg, error);
+        res.status(422).json(errorMsg)
+    });
+
+    const filteredImage = await filterImageFromURL(imageData)
     .then((path) => {
         console.log("Filtered image path:", path);
         return path;
     })
     .catch((error) => {
-        console.error("Error processing image:", error);
+        errorMsg = "Error processing image:";
+        console.error(errorMsg, error);
+        res.status(422).json(errorMsg)
     });
 
     if(!filteredImage) {
-        const errorFilteringImage = "Image not filtered";
-        console.error(errorFilteringImage);
-        res.status(422).json(errorFilteringImage);
+        errorMsg = "Image not filtered";
+        console.error(errorMsg);
+        res.status(422).json(errorMsg);
     }
 
-
-    await fs.exists(filteredImage, (exists) => {
-        if(!exists) {
-            res.status(404).json("Image does not exist")
-        }
-        fs.readFile(filteredImage, (err, content) => {
-            res.status(200).end(content);
-        })
-    });
+    res.sendFile(filteredImage);
 
 });
--- util/util.js
+++ util/util.js
@@ -1,33 +1,32 @@
 import fs from "fs";
 import Jimp from "jimp";
-import axios from 'axios';
 
-export async function filterImageFromURL(inputURL) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await axios.get(inputURL, { responseType: 'arraybuffer' });
-            console.log('Content Type:', response.headers['content-type']);
 
-            if (!response.headers['content-type'].startsWith('image/')) {
-                reject(new Error('URL did not return an image'));
-                return;
-            }
-            const photo = await Jimp.read(Buffer.from(response.data));
-
-            const outpath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-            await photo
-                .resize(256, 256) 
-                .quality(60)
-                .greyscale() 
-                .write(outpath, (img) => {
-                    resolve(outpath);
-                });
-        } catch (error) {
-            reject(error);
-        }
-    });
+// filterImageFromURL
+// helper function to download, filter, and save the filtered image locally
+// returns the absolute path to the local image
+// INPUTS
+//    inputURL: string - a publicly accessible url to an image file
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+ export async function filterImageFromURL(inputURL) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const photo = await Jimp.read(inputURL);
+      const outpath =
+        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      await photo
+        .resize(256, 256) // resize
+        .quality(60) // set JPEG quality
+        .greyscale() // set greyscale
+        .write(outpath, (img) => {
+          resolve(outpath);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
-
 
 // deleteLocalFiles
 // helper function to delete files on the local disk
